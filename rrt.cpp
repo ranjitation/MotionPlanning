4768171514688191a024718b18db571cd93d8d59
Ranjith <ranjitation@gmail.com>
2020-02-11 11:09:41 -0600

    Update rrt.cpp

--- rrt.cpp
+++ rrt.cpp
@@ -1,13 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cmath>
+#include <random>
+#include <tuple>
+#include <chrono>
+
+unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+std::default_random_engine gen(seed);
+std::uniform_real_distribution<double> distribution(-50.0, 50.0);
 
 class vertex
 {
-    protected:
+    public:
         float x;
         float y;
-    public:
         vertex(float _x = 0, float _y = 0)
         {
             x = _x;
@@ -22,53 +29,50 @@ class vertex
 class edge
 {
     protected:
-        vertex* from;
-        vertex* to;
+        vertex from;
+        vertex to;
         float dist;
     public:
-        edge(vertex &f, vertex &t,const float d)
+        edge(vertex& f, vertex& t) : from(f), to(t)
         {
-            from = &f;
-            to = &t;
-            dist = d;
+            dist = std::sqrt(std::pow(to.x-from.x, 2)+std::pow(to.y-from.y, 2));
         }
         void getedge()
         {
-            from->getvertex();
+            from.getvertex();
             std::cout << '-' << dist << "->";
-            to->getvertex();
+            to.getvertex();
         }
 
 };
 
 class graph
 {
-    protected:
+    public:
         float length;
         float width;
         std::vector<vertex> vertices;
         std::vector<edge> edges;
-    public:
         graph(float l = 100, float w = 100)
         {
             length = l;
             width = w;
         }
-        void addvertex(const vertex &v1)
+        void addvertex(const vertex& v1)
         {
             vertices.push_back(v1);
         }
-        void addedges(const edge &e1)
+        void addedges(const edge& e1)
         {
             edges.push_back(e1);
         }
         void showgraph()
         {
             std::cout << "Vertices: " << '[';
-            for (int i=0; i<vertices.size(); ++i)
+            for (int i = 0; i < vertices.size(); ++i)
             {
                 vertices[i].getvertex();
-                if (i != vertices.size()-1)
+                if (i != vertices.size() - 1)
                 {
                     std::cout << ", ";
                 }
@@ -76,35 +80,76 @@ class graph
             std::cout << ']' << std::endl;
 
             std::cout << "Edges: " << '[';
-            for (int i=0; i<edges.size(); ++i)
+            for (int i = 0; i < edges.size(); ++i)
             {
                 edges[i].getedge();
-                if (i != edges.size()-1)
+                if (i != edges.size() - 1)
                 {
-                    std::cout << ", ";
+                    std::cout << ", " << std::endl;
                 }
             }
             std::cout << "]\n";
         }
-
 };
+
+std::tuple<vertex, float> closest(vertex& new_vert, graph& g)
+{
+    float d = 100000000;
+    vertex final;
+    for (auto v : g.vertices)
+    {
+        if (std::sqrt(std::pow(new_vert.x - v.x, 2) + std::pow(new_vert.y - v.y, 2)) < d)
+        {
+            d = std::sqrt(std::pow(new_vert.x - v.x, 2) + std::pow(new_vert.y - v.y, 2));
+            final = v;
+        }
+    }
+    std::tuple<vertex, float> tup;
+    tup = std::make_tuple(final, d);
+    return tup;
+}
+
+void rrt(vertex& start, vertex& goal, int num_of_nodes, graph& g)
+{
+    bool done = false;
+    for (int i = 0; i < num_of_nodes; ++i)
+    {
+        vertex new_vert(distribution(gen), distribution(gen));
+        std::tuple<vertex, float> tup = closest(new_vert, g);
+        vertex v = std::get<0>(tup);
+        float d = std::get<1>(tup);
+            
+        if (d > 2)
+        {
+            float slope = (new_vert.y - v.y) / (new_vert.x - v.x);
+            new_vert.x = std::sqrt(4.0/(std::pow(slope,2)+1.0)) + v.x;
+            new_vert.y = slope * (new_vert.x-v.x) + v.y;
+        }
+        if (std::sqrt(std::pow(new_vert.x - goal.x, 2) + std::pow(new_vert.y - goal.y, 2)) <= 3)
+        {
+            done = true;
+        }
+        g.addvertex(new_vert);
+        edge e0(v, new_vert);
+        g.addedges(e0);
+        if (done)
+        {
+            break;
+        }
+    }
+}
 
 int main()
 {
-    vertex v0(1,2);
-    vertex v1(2,3);
-    edge e0(v0, v1, 5.4);
-    e0.getedge();
     graph g;
-    g.addvertex(v0);
-    g.addvertex(v1);
-    g.addedges(e0);
-    g.showgraph();
-    // for (auto x : {1,2,3,4,5})
-    // {
-    //     std::cout << x << ',';
-    // }
-    // std::cout << std::endl;
+    vertex start(0, 0);
+    vertex goal(10, 10);
+    g.addvertex(start);
+    int num_of_nodes = 500;
+    rrt(start, goal, num_of_nodes, g);
+    //g.showgraph();
+    std::cout << g.vertices.size() << std::endl;
+    std::cout << g.edges.size() << std::endl;
 
     return 0;
 }
